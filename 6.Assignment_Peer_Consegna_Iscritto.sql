2360db244c79d445da224725e1e91d037bc316c9
a-magno98 <80479517+a-magno98@users.noreply.github.com>
2022-10-18 16:35:18 +0200

    Add files via upload

---
+++ 6.Assignment_Peer_Consegna_Iscritto.sql
@@ -0,0 +1,181 @@
+﻿set search_path to "progetto";
+
+INSERT INTO Assignment VALUES('IE100', 'IN001', '07/03/2019 00:00:00', '13/03/2019  00:00:00', 'E', 01, 'info01', 18, DEFAULT);
+INSERT INTO Assignment VALUES('IE101', 'IN002','14/03/2019 00:00:00', '20/03/2019 00:00:00', 'E', 01, 'info01', 18, DEFAULT);
+INSERT INTO Assignment VALUES('IE102', 'IN003', '21/03/2019 00:00:00', '27/03/2019 00:00:00', 'E', 01, 'info01', 18, DEFAULT);
+INSERT INTO Assignment VALUES('IQ100', 'IN004', '28/03/2019 00:00:00', '03/04/2019 00:00:00', 'Q', 01, 'info01', 15, DEFAULT);
+INSERT INTO Assignment VALUES('IE103', 'IN005', '04/04/2019 00:00:00', '10/04/2019 00:00:00', 'E', 01, 'info01', 16, DEFAULT);
+
+INSERT INTO Assignment VALUES('IE200', 'IN101','07/04/2019 00:00:00', '13/04/2019 00:00:00', 'E', 01, 'info02',16, DEFAULT);
+INSERT INTO Assignment VALUES('IE201', 'IN102','14/04/2019 00:00:00', '20/04/2019 00:00:00', 'E', 01, 'info02',16, DEFAULT);
+INSERT INTO Assignment VALUES('IE202', 'IN103','21/04/2019 00:00:00', '27/04/2019 00:00:00', 'E', 01, 'info02',16, DEFAULT);
+INSERT INTO Assignment VALUES('IQ200', 'IN104','28/04/2019 00:00:00', '03/05/2019 00:00:00', 'Q', 01, 'info02',15, DEFAULT);
+INSERT INTO Assignment VALUES('IE203', 'IN105','04/05/2019 00:00:00', '10/05/2019 00:00:00', 'E', 01, 'info02',16, DEFAULT);
+INSERT INTO Assignment VALUES('IE204', 'IN106','11/05/2019 00:00:00', '17/05/2019 00:00:00', 'E', 01, 'info02',16, TRUE);
+
+INSERT INTO Assignment VALUES('IQ300', DEFAULT, '21/03/2019 00:00:00', DEFAULT, 'Q', 01, 'info03',17, DEFAULT);
+INSERT INTO Assignment VALUES('IQ301', DEFAULT,'28/03/2019 00:00:00', DEFAULT, 'Q', 01, 'info03',17, DEFAULT);
+INSERT INTO Assignment VALUES('IQ302', DEFAULT,'04/04/2019 00:00:00', DEFAULT, 'Q', 01, 'info03',17, DEFAULT);
+INSERT INTO Assignment VALUES('IQ303', DEFAULT,'21/03/2019 00:00:00', DEFAULT, 'Q', 01, 'info03',16, DEFAULT);
+INSERT INTO Assignment VALUES('IQ304', DEFAULT,'28/03/2019 00:00:00', DEFAULT, 'Q', 01, 'info03',16, DEFAULT);
+INSERT INTO Assignment VALUES('IQ305', DEFAULT,'04/04/2019 00:00:00', DEFAULT, 'Q', 01, 'info03',16, DEFAULT);
+
+INSERT INTO Assignment VALUES('ME200', 'MA101', '16/03/2019 00:00:00', '22/03/2019 00:00:00', 'E', 01, 'mate02',18, DEFAULT);
+INSERT INTO Assignment VALUES('ME201', 'MA102','23/03/2019 00:00:00', '29/03/2019 00:00:00', 'E', 01, 'mate02',18, DEFAULT);
+INSERT INTO Assignment VALUES('ME202', 'MA103','30/03/2019 00:00:00', '05/04/2019 00:00:00', 'E', 01, 'mate02',18, DEFAULT);
+INSERT INTO Assignment VALUES('ME203', 'MA104','06/04/2019 00:00:00', '12/04/2019 00:00:00', 'E', 01, 'mate02',18, DEFAULT);
+
+INSERT INTO Assignment VALUES('ME300', 'MA201', '17/05/2019 00:00:00', '23/05/2019 00:00:00', 'E', 01, 'mate03',15, DEFAULT);
+INSERT INTO Assignment VALUES('ME301', 'MA202', '24/05/2019 00:00:00', '30/05/2019 00:00:00', 'E', 01, 'mate03',15, DEFAULT);
+INSERT INTO Assignment VALUES('ME302', 'MA203', '31/05/2019 00:00:00', '06/06/2019 00:00:00', 'E', 01, 'mate03',15, TRUE);
+INSERT INTO Assignment VALUES('ME303', 'MA204', '07/06/2019 00:00:00', '13/06/2019 00:00:00', 'E', 01, 'mate03',15, TRUE);
+INSERT INTO Assignment VALUES('ME304', 'MA205', '14/06/2019 00:00:00', '20/06/2019 00:00:00', 'E', 01, 'mate03',15, DEFAULT);
+INSERT INTO Assignment VALUES('ME305', 'MA206', '21/06/2019 00:00:00', '27/06/2019 00:00:00', 'E', 01, 'mate03',15, DEFAULT);
+
+INSERT INTO Assignment VALUES('FQ100', DEFAULT, '22/03/2019 00:00:00', DEFAULT, 'Q', 01, 'fis01',15, DEFAULT);
+INSERT INTO Assignment VALUES('FQ101', DEFAULT, '29/03/2019 00:00:00', DEFAULT, 'Q', 01, 'fis01',15, DEFAULT);
+INSERT INTO Assignment VALUES('FQ102', DEFAULT, '05/04/2019 00:00:00', DEFAULT, 'Q', 01, 'fis01',15, DEFAULT);
+INSERT INTO Assignment VALUES('FQ103', DEFAULT, '12/04/2019 00:00:00', DEFAULT, 'Q', 01, 'fis01',15, DEFAULT);
+INSERT INTO Assignment VALUES('FQ104', DEFAULT, '19/04/2019 00:00:00', DEFAULT, 'Q', 01, 'fis01',15, DEFAULT);
+INSERT INTO Assignment VALUES('FQ105', DEFAULT, '26/04/2019 00:00:00', DEFAULT, 'Q', 01, 'fis01',15, DEFAULT);
+
+INSERT INTO Assignment VALUES('FE300', 'FI201', '07/02/2019 00:00:00', '13/02/2019 00:00:00', 'E', 01, 'fis03',18, TRUE);
+INSERT INTO Assignment VALUES('FE301', 'FI202', '14/02/2019 00:00:00', '20/02/2019 00:00:00', 'E', 01, 'fis03',18, TRUE);
+INSERT INTO Assignment VALUES('FE302', 'FI203', '21/02/2019 00:00:00', '27/02/2019 00:00:00', 'E', 01, 'fis03',18, TRUE);
+INSERT INTO Assignment VALUES('FE303', 'FI204', '28/02/2019 00:00:00', '06/03/2019 00:00:00', 'E', 01, 'fis03',18, TRUE);
+
+INSERT INTO Assignment VALUES('CE100', 'CH001', '05/05/2019 00:00:00', '11/05/2019 00:00:00', 'E', 01, 'chim01',18, DEFAULT);
+INSERT INTO Assignment VALUES('CE101', 'CH002', '12/05/2019 00:00:00', '18/05/2019 00:00:00', 'E', 01, 'chim01',18, DEFAULT);
+INSERT INTO Assignment VALUES('CE102', 'CH003', '19/05/2019 00:00:00', '25/05/2019 00:00:00', 'E', 01, 'chim01',18, DEFAULT);
+INSERT INTO Assignment VALUES('CQ100', 'CH004', '26/05/2019 00:00:00', '01/06/2019 00:00:00', 'Q', 01, 'chim01',15, DEFAULT);
+
+INSERT INTO Iscritto VALUES('lorenzo.mancini@unige.it', 'info01', 01, DEFAULT,'28/02/2019');
+INSERT INTO Iscritto VALUES('martina.cocci@unige.it', 'info01', 01, DEFAULT, '27/02/2019');
+INSERT INTO Iscritto VALUES('ginevra.torre@unige.it', 'fis01', 01, DEFAULT, '15/03/2019');
+--INSERT INTO Iscritto VALUES('bacco.baresi@unito.it', 'fis02', 01, DEFAULT, '16/03/2019');
+INSERT INTO Iscritto VALUES('venerando.palermi@unimi.it', 'mate02', 01, DEFAULT, '09/03/2019');
+INSERT INTO Iscritto VALUES('arianna.lifonti@unimi.it', 'chim01', 01, DEFAULT, '15/04/2019');
+INSERT INTO Iscritto VALUES('gino.beneventi@univda.it', 'info02', 01, DEFAULT, '01/04/2019');
+--INSERT INTO Iscritto VALUES('emilia.sal@univda.it', 'chim02', 01, DEFAULT, '05/05/2019');
+INSERT INTO Iscritto VALUES('susanna.moretti@unifi.it', 'fis03', 01, DEFAULT, '31/01/2019');
+INSERT INTO Iscritto VALUES('giulia.romani@unifi.it', 'mate03', 01, DEFAULT, '11/05/2019');
+--INSERT INTO Iscritto VALUES('giorgia.lombardi@unibo.it', 'info04', 01, DEFAULT, '07/07/2019');
+--INSERT INTO Iscritto VALUES('dorotea.zetticci@unibo.it', 'info04', 01, DEFAULT, '11/07/2019');
+INSERT INTO Iscritto VALUES('ornella.nucci@unige.it', 'info01', 01, DEFAULT, '03/03/2019');
+INSERT INTO Iscritto VALUES('giuditta.fani@unige.it', 'info01', 01, DEFAULT, '28/02/2019');
+--INSERT INTO Iscritto VALUES('leo.padovesi@unito.it', 'fis02', 01, DEFAULT, '07/07/2019');
+--INSERT INTO Iscritto VALUES('fabrizio.curi@unimi.it', 'fis02', 01, DEFAULT, '07/07/2019');
+--INSERT INTO Iscritto VALUES('silvia.bergamaschi@unimi.it', 'mate01',01, DEFAULT, '01/04/2019');
+--INSERT INTO Iscritto VALUES('luca.mazzanti@unibo.it', 'chim02', 01, DEFAULT, '07/05/2019');
+--INSERT INTO Iscritto VALUES('girolamo.moretti@unibo.it', 'chim02', 01, DEFAULT, '07/06/2019');
+INSERT INTO Iscritto VALUES('isidoro.costa@unifi.it', 'fis03', 01, DEFAULT, '28/01/2019');
+INSERT INTO Iscritto VALUES('filippo.turati@univda.it', 'info02', 01, DEFAULT, '31/03/2019');
+INSERT INTO Iscritto VALUES('gianmarco.poli@univda.it', 'info02', 01, DEFAULT, '30/03/2019');
+INSERT INTO Iscritto VALUES('marco.lucchese@unige.it', 'info01', 01, DEFAULT, '27/02/2019');
+INSERT INTO Iscritto VALUES('davide.liviano@unige.it', 'info01', 01, DEFAULT, '26/02/2019');
+INSERT INTO Iscritto VALUES('emma.polisi@unifi.it', 'info01', 01, DEFAULT, '26/02/2019');
+INSERT INTO Iscritto VALUES('jacopo.cremonesi@unige.it', 'info02', 01, DEFAULT, '29/03/2019');
+INSERT INTO Iscritto VALUES('carmela.rossi@unige.it', 'info02', 01, DEFAULT, '28/03/2019');
+INSERT INTO Iscritto VALUES('serena.lofere@univda.it', 'info02', 01, DEFAULT, '27/03/2019');
+INSERT INTO Iscritto VALUES('elena.monaldo@unito.it', 'info03', 01, DEFAULT, '04/03/2019');
+INSERT INTO Iscritto VALUES('stefania.dipasqui@unige.it', 'info03', 01, DEFAULT, '03/03/2019');
+INSERT INTO Iscritto VALUES('enrico.lasdi@unige.it', 'info03', 01, DEFAULT, '05/03/2019');
+INSERT INTO Iscritto VALUES('giulio.molsi@unito.it', 'mate02', 01, DEFAULT, '09/03/2019');
+INSERT INTO Iscritto VALUES('filippo.bianchi@unimi.it', 'mate03', 01, DEFAULT, '10/05/2019');
+INSERT INTO Iscritto VALUES('fiamma.fanucci@unimi.it', 'mate03', 01, DEFAULT, '09/05/2019');
+INSERT INTO Iscritto VALUES('iolanda.russo@unimi.it', 'fis01', 01, DEFAULT, '15/03/2019');
+INSERT INTO Iscritto VALUES('dionisio.fiorentino@unimi.it', 'fis01', 01, DEFAULT, '14/03/2019');
+--INSERT INTO Iscritto VALUES('giuliano.brusti@unimi.it', 'fis02', 01, DEFAULT, '16/05/2019');
+--INSERT INTO Iscritto VALUES('isotta.torti@unibo.it', 'fis02', 01, DEFAULT, '15/05/2019');
+INSERT INTO Iscritto VALUES('christian.trentino@unibo.it', 'fis03', 01, DEFAULT, '29/01/2019');
+INSERT INTO Iscritto VALUES('tiziano.napolitani@unibo.it', 'fis03', 01, DEFAULT, '28/01/2019');
+INSERT INTO Iscritto VALUES('simone.ossico@unibo.it', 'chim01', 01, DEFAULT, '27/04/2019');
+INSERT INTO Iscritto VALUES('ernesto.bruno@unifi.it', 'chim01', 01, DEFAULT, '26/04/2019');
+--INSERT INTO Iscritto VALUES('rossana.trevisani@unifi.it', 'chim02', 01, DEFAULT, '05/05/2019');
+--INSERT INTO Iscritto VALUES('lino.pisano@unifi.it', 'chim02', 01, DEFAULT, '07/07/2019');
+
+
+
+INSERT INTO Consegna VALUES(30,'02/03/2019 12:00:02','marco.lucchese@unige.it', 'IE100', DEFAULT);
+INSERT INTO Consegna VALUES(29,'09/03/2019 11:45:03','marco.lucchese@unige.it', 'IE101', DEFAULT);
+INSERT INTO Consegna VALUES(30,'16/03/2019 10:39:45','marco.lucchese@unige.it', 'IE102', DEFAULT);
+INSERT INTO Consegna VALUES(30,'30/03/2019 15:45:46','marco.lucchese@unige.it', 'IE103', DEFAULT);
+INSERT INTO Consegna VALUES(25,'03/03/2019 16:45:59','davide.liviano@unige.it', 'IE100', DEFAULT);
+INSERT INTO Consegna VALUES(24,'10/03/2019 13:20:36','davide.liviano@unige.it', 'IE101', DEFAULT);
+INSERT INTO Consegna VALUES(23,'17/03/2019 10:25:41','davide.liviano@unige.it', 'IE102', DEFAULT);
+INSERT INTO Consegna VALUES(23,'31/03/2019 14:32:06','davide.liviano@unige.it', 'IE103', DEFAULT);
+INSERT INTO Consegna VALUES(29,'01/03/2019 19:15:13','emma.polisi@unifi.it', 'IE100', DEFAULT);
+INSERT INTO Consegna VALUES(29,'08/03/2019 13:25:14','emma.polisi@unifi.it', 'IE101', DEFAULT);
+INSERT INTO Consegna VALUES(29,'15/03/2019 16:14:18','emma.polisi@unifi.it', 'IE102', DEFAULT);
+INSERT INTO Consegna VALUES(29,'29/03/2019 13:46:46','emma.polisi@unifi.it', 'IE103', DEFAULT);
+
+INSERT INTO Consegna VALUES(23, '03/04/2019 19:13:45', 'jacopo.cremonesi@unige.it', 'IE200', DEFAULT);
+INSERT INTO Consegna VALUES(19, '10/04/2019 19:10:13', 'jacopo.cremonesi@unige.it', 'IE201', DEFAULT);
+INSERT INTO Consegna VALUES(25, '16/04/2019 19:16:15', 'jacopo.cremonesi@unige.it', 'IE202', DEFAULT);
+INSERT INTO Consegna VALUES(25, '01/05/2019 19:25:02', 'jacopo.cremonesi@unige.it', 'IE203', DEFAULT);
+INSERT INTO Consegna VALUES(24, '09/05/2019 19:30:05', 'jacopo.cremonesi@unige.it', 'IE204', DEFAULT);
+INSERT INTO Consegna VALUES(29, '01/04/2019 19:29:04', 'carmela.rossi@unige.it', 'IE200', DEFAULT);
+INSERT INTO Consegna VALUES(28, '08/04/2019 19:24:06', 'carmela.rossi@unige.it', 'IE201', DEFAULT);
+INSERT INTO Consegna VALUES(29, '15/04/2019 19:27:05', 'carmela.rossi@unige.it', 'IE202', DEFAULT);
+INSERT INTO Consegna VALUES(29, '03/05/2019 19:07:56', 'carmela.rossi@unige.it', 'IE203', DEFAULT);
+INSERT INTO Consegna VALUES(28, '09/05/2019 19:08:53', 'carmela.rossi@unige.it', 'IE204', DEFAULT);
+INSERT INTO Consegna VALUES(25, '01/04/2019 19:07:53', 'serena.lofere@univda.it', 'IE200', DEFAULT);
+INSERT INTO Consegna VALUES(26, '08/04/2019 19:43:07', 'serena.lofere@univda.it', 'IE201', DEFAULT);
+INSERT INTO Consegna VALUES(28, '15/04/2019 19:48:07', 'serena.lofere@univda.it', 'IE202', DEFAULT);
+INSERT INTO Consegna VALUES(27, '30/04/2019 19:52:07', 'serena.lofere@univda.it', 'IE203', DEFAULT);
+INSERT INTO Consegna VALUES(29, '06/05/2019 19:53:09', 'serena.lofere@univda.it', 'IE204', DEFAULT);
+
+INSERT INTO Consegna VALUES(16, '11/03/2019 15:05:06', 'giulio.molsi@unito.it', 'ME200', DEFAULT);
+INSERT INTO Consegna VALUES(18, '12/03/2019 15:07:09', 'giulio.molsi@unito.it', 'ME200', DEFAULT);
+INSERT INTO Consegna VALUES(15, '17/03/2019 15:35:07', 'giulio.molsi@unito.it', 'ME201', DEFAULT);
+INSERT INTO Consegna VALUES(15, '18/03/2019 15:40:09', 'giulio.molsi@unito.it', 'ME201', DEFAULT);
+INSERT INTO Consegna VALUES(18, '18/03/2019 17:30:55', 'giulio.molsi@unito.it', 'ME201', DEFAULT);
+INSERT INTO Consegna VALUES(19, '25/03/2019 17:30:55', 'giulio.molsi@unito.it', 'ME202', DEFAULT);
+INSERT INTO Consegna VALUES(18, '31/03/2019 18:36:55', 'giulio.molsi@unito.it', 'ME203', DEFAULT);
+
+INSERT INTO Consegna VALUES(14, '11/05/2019 16:04:08', 'filippo.bianchi@unimi.it', 'ME300', DEFAULT);
+INSERT INTO Consegna VALUES(28, '11/05/2019 19:04:12', 'filippo.bianchi@unimi.it', 'ME300', DEFAULT);
+INSERT INTO Consegna VALUES(20, '19/05/2019 16:40:08', 'filippo.bianchi@unimi.it', 'ME301', DEFAULT);
+INSERT INTO Consegna VALUES(24, '25/05/2019 19:36:04', 'filippo.bianchi@unimi.it', 'ME302', DEFAULT);
+INSERT INTO Consegna VALUES(20, '01/06/2019 12:04:52', 'filippo.bianchi@unimi.it', 'ME303', DEFAULT);
+INSERT INTO Consegna VALUES(26, '09/06/2019 18:07:56', 'filippo.bianchi@unimi.it', 'ME304', DEFAULT);
+INSERT INTO Consegna VALUES(24, '16/06/2019 17:15:36', 'filippo.bianchi@unimi.it', 'ME305', DEFAULT);
+INSERT INTO Consegna VALUES(28, '11/05/2019 18:05:12', 'fiamma.fanucci@unimi.it', 'ME300', DEFAULT);
+INSERT INTO Consegna VALUES(28, '19/05/2019 19:40:08', 'fiamma.fanucci@unimi.it', 'ME301', DEFAULT);
+INSERT INTO Consegna VALUES(29, '25/05/2019 12:36:04', 'fiamma.fanucci@unimi.it', 'ME302', DEFAULT);
+INSERT INTO Consegna VALUES(27, '01/06/2019 11:09:52', 'fiamma.fanucci@unimi.it', 'ME303', DEFAULT);
+INSERT INTO Consegna VALUES(26, '09/06/2019 13:16:56', 'fiamma.fanucci@unimi.it', 'ME304', DEFAULT);
+INSERT INTO Consegna VALUES(29, '16/06/2019 19:15:07', 'fiamma.fanucci@unimi.it', 'ME305', DEFAULT);
+
+INSERT INTO Consegna VALUES(30, '02/02/2019 09:13:54', 'christian.trentino@unibo.it', 'FE300', DEFAULT);
+INSERT INTO Consegna VALUES(30, '08/02/2019 09:16:55', 'christian.trentino@unibo.it', 'FE301', DEFAULT);
+INSERT INTO Consegna VALUES(30, '15/02/2019 09:19:59', 'christian.trentino@unibo.it', 'FE302', DEFAULT);
+INSERT INTO Consegna VALUES(30, '22/02/2019 09:17:32', 'christian.trentino@unibo.it', 'FE303', DEFAULT);
+INSERT INTO Consegna VALUES(29, '01/02/2019 10:13:54', 'tiziano.napolitani@unibo.it', 'FE300', DEFAULT);
+INSERT INTO Consegna VALUES(25, '08/02/2019 10:16:55', 'tiziano.napolitani@unibo.it', 'FE301', DEFAULT);
+INSERT INTO Consegna VALUES(26, '15/02/2019 10:19:59', 'tiziano.napolitani@unibo.it', 'FE302', DEFAULT);
+INSERT INTO Consegna VALUES(28, '22/02/2019 10:17:32', 'tiziano.napolitani@unibo.it', 'FE303', DEFAULT);
+
+INSERT INTO Consegna VALUES(25, '29/04/2019 15:46:18', 'simone.ossico@unibo.it', 'CE100', DEFAULT);
+INSERT INTO Consegna VALUES(29, '19/05/2019 17:23:04', 'simone.ossico@unibo.it', 'CE101', DEFAULT);
+INSERT INTO Consegna VALUES(26, '26/05/2019 14:30:47', 'simone.ossico@unibo.it', 'CE102', DEFAULT);
+
+INSERT INTO Consegna VALUES(18, '29/04/2019 10:25:36', 'ernesto.bruno@unifi.it', 'CE100', DEFAULT);
+INSERT INTO Consegna VALUES(18, '06/05/2019 10:20:16', 'ernesto.bruno@unifi.it', 'CE101', DEFAULT);
+INSERT INTO Consegna VALUES(18, '13/05/2019 10:18:05', 'ernesto.bruno@unifi.it', 'CE102', DEFAULT);
+
+INSERT INTO Peer VALUES('FE300', 'christian.trentino@unibo.it', '02/02/2019 09:13:54', 'tiziano.napolitani@unibo.it', 'susanna.moretti@unifi.it', '09/02/2019 09:00:00', 28,0.30, '09/02/2019 17:52:30');
+INSERT INTO Peer VALUES('FE301', 'christian.trentino@unibo.it', '08/02/2019 09:16:55', 'tiziano.napolitani@unibo.it', 'susanna.moretti@unifi.it', '16/02/2019 09:00:00', 30,0.30, '17/02/2019 09:30:04');
+INSERT INTO Peer VALUES('FE302', 'christian.trentino@unibo.it', '15/02/2019 09:19:59', 'tiziano.napolitani@unibo.it', 'susanna.moretti@unifi.it', '22/02/2019 09:00:00', 29,0.30, '23/02/2019 10:55:00');
+INSERT INTO Peer VALUES('FE303', 'christian.trentino@unibo.it', '22/02/2019 09:17:32', 'tiziano.napolitani@unibo.it', 'susanna.moretti@unifi.it', '01/03/2019 09:00:00', 29,0.30, '02/03/2019 15:16:17');
+INSERT INTO Peer VALUES('FE300', 'tiziano.napolitani@unibo.it', '01/02/2019 10:13:54', 'christian.trentino@unibo.it', 'susanna.moretti@unifi.it', '09/02/2019 09:00:00', 28,0.30, '11/02/2019 16:12:32');
+INSERT INTO Peer VALUES('FE301', 'tiziano.napolitani@unibo.it', '08/02/2019 10:16:55','christian.trentino@unibo.it', 'susanna.moretti@unifi.it', '16/02/2019 09:00:00', 24,0.30, '18/02/2019 16:34:48');
+INSERT INTO Peer VALUES('FE302', 'tiziano.napolitani@unibo.it', '15/02/2019 10:19:59', 'christian.trentino@unibo.it','susanna.moretti@unifi.it', '22/02/2019 09:00:00', 25,0.30, '24/02/2019 16:15:19');
+INSERT INTO Peer VALUES('FE303', 'tiziano.napolitani@unibo.it', '22/02/2019 10:17:32', 'christian.trentino@unibo.it', 'susanna.moretti@unifi.it', '01/03/2019 09:00:00', 24,0.30, '03/03/2019 16:18:20');
+INSERT INTO Peer VALUES('ME302', 'fiamma.fanucci@unimi.it', '25/05/2019 12:36:04', 'filippo.bianchi@unimi.it', 'giulia.romani@unifi.it', '01/06/2019 08:30:00', 25, 0.30, '04/06/2019 16:07:05');
+INSERT INTO Peer VALUES('ME303', 'fiamma.fanucci@unimi.it', '01/06/2019 11:09:52', 'filippo.bianchi@unimi.it', 'giulia.romani@unifi.it', '08/06/2019 08:30:00', 26, 0.30, '10/06/2019 13:08:16');
+INSERT INTO Peer VALUES('ME302', 'filippo.bianchi@unimi.it', '25/05/2019 19:36:04', 'fiamma.fanucci@unimi.it', 'giulia.romani@unifi.it', '01/06/2019 08:30:00', 23, 0.30, '02/06/2019 17:18:19');
+INSERT INTO Peer VALUES('ME303', 'filippo.bianchi@unimi.it', '01/06/2019 12:04:52', 'fiamma.fanucci@unimi.it',  'giulia.romani@unifi.it', '08/06/2019 08:30:00', 24, 0.30, '09/06/2019 16:15:13');
+INSERT INTO Peer VALUES('IE204', 'serena.lofere@univda.it', '06/05/2019 19:53:09', 'jacopo.cremonesi@unige.it', 'gino.beneventi@univda.it', '12/05/2019 08:30:00', 28, 0.50, '13/05/2019 19:18:20');
+INSERT INTO Peer VALUES('IE204', 'jacopo.cremonesi@unige.it', '09/05/2019 19:30:05', 'carmela.rossi@unige.it', 'gino.beneventi@univda.it', '12/05/2019 08:30:00', 29, 0.50, '14/05/2019 16:07:18');
+INSERT INTO Peer VALUES('IE204', 'carmela.rossi@unige.it', '09/05/2019 19:08:53', 'serena.lofere@univda.it', 'gino.beneventi@univda.it', '12/05/2019 08:30:00', 30, 0.50, '12/05/2019 16:03:20');
